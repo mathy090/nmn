ad889e1c8a67f12ed1b879db76fe82f512ec5c51
mathy090 <tafadzwarunowanda@gmail.com>
2025-08-16 14:18:14 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
 const { validateLogin } = require('../middleware/validation');
-const { login, getProfile } = require('../controllers/authController');
+const { login, signup, getProfile } = require('../controllers/authController');
 
 // Public routes
 router.post('/login', validateLogin, login);
+router.post('/signup', signup);
 
 // Protected routes
 router.get('/profile', protect, getProfile);
