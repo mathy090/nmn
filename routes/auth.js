f98ab52c906a87e15d876fc841127a9d32276da1
mathy090 <tafadzwarunowanda@gmail.com>
2025-08-17 17:51:30 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,14 +8,12 @@ const {
   login,
   signup,
   getProfile,
-  forgotPassword,
-  resetPassword // Import the new functions
+  forgotPassword, // Import the new functions
+  resetPassword   // Import the new functions
 } = require('../controllers/authController');
 
 // Public routes
-// router.post('/login', validateLogin, login); // Add validation if you have it
 router.post('/login', login);
-// router.post('/signup', validateSignup, signup); // Add validation if you have it
 router.post('/signup', signup);
 router.post('/forgot-password', forgotPassword); // New route for forgot password
 router.put('/reset-password/:token', resetPassword); // New route for reset password (PUT or POST)
