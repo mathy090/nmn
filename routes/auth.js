728c9fb77be24a3be4262e527f2f118625dce548
mathy090 <tafadzwarunowanda@gmail.com>
2025-08-25 18:10:58 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 // @route   POST /api/auth/login
