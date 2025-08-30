c37cd783151386a219710f323350277a3d3e5db7
mathy090 <tafadzwarunowanda@gmail.com>
2025-08-30 16:42:03 +0200

    Update sync.js

--- routes/sync.js
+++ routes/sync.js
@@ -1,14 +1,10 @@
-// routes/sync.js
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const { syncRecords, getUnsyncedRecords } = require('../controllers/syncController');
+const { syncOfflineRecords, getUnsyncedRecords } = require('../controllers/syncController');
 
-// All routes require authentication
 router.use(protect);
-
-// Sync routes
-router.post('/', syncRecords);
+router.post('/', syncOfflineRecords);
 router.get('/unsynced', getUnsyncedRecords);
 
 module.exports = router;
